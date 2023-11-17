d086f38fe2b0a277b588577e4276709294f16041
dishitamohan <92173465+dishitamohan@users.noreply.github.com>
2023-11-17 07:49:53 +0530

    Update display.php

--- test2/display.php
+++ test2/display.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<!--<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Display Data</title>
@@ -28,4 +28,39 @@
     </div>
 
 </body>
+</html> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Display Data</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <!-- Add a link to a new Tailwind CSS theme -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind-new-theme.min.css" rel="stylesheet">
+</head>
+<body class="bg-gray-800 min-h-screen flex items-center justify-center text-white">
+
+    <div class="bg-purple-700 p-8 rounded shadow-md w-full max-w-md">
+        <h1 class="text-2xl font-bold mb-4">Data Display</h1>
+        <?php
+        $data = file_get_contents("data.txt");
+
+        if ($data !== false) {
+            $entries = explode("\n", $data);
+
+            foreach ($entries as $entry) {
+                if (!empty($entry)) {
+                    echo "<p class='mb-2'>$entry</p>";
+                }
+            }
+        } else {
+            echo "<p>No data available.</p>";
+        }
+        ?>
+    </div>
+
+</body>
 </html>
+
